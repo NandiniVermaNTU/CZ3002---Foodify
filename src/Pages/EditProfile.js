e334543699d3488a1c8b60e991a3a68275ad780c
Serylvan <61868529+Serylvan@users.noreply.github.com>
2022-10-25 00:10:55 +0800

    Profile

--- src/Pages/EditProfile.js
+++ src/Pages/EditProfile.js
@@ -52,7 +52,7 @@ fetchUserName();
                 </div> */}
                 <div class="mb-6">
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Registered Email*</label>
-                    <input id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="abc@gmail.com" required></input>
+                    <input id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={user?.email} required></input>
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Name</label>
--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import { auth, db, logout } from "../firebase/firebase.js";
+import { query, collection, getDocs, where } from "firebase/firestore";
+//
 //import { Link } from "react-router-dom";
 import "../PagesCSS/Home.css";
 import pic1 from "../Images/FoodifyBckgd.jpg";
@@ -13,6 +18,27 @@ let activeStyle = {
 
 
 export default function Profile() {
+  const [user, loading, error] = useAuthState(auth);
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const navigate = useNavigate();
+  const fetchUserName = async () => {
+  try {
+      const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+      const doc = await getDocs(q);
+      const data = doc.docs[0].data();
+      setName(data.name);
+      setPhone(data.phone);
+  } catch (err) {
+      console.error(err);
+      alert("An error occured while fetching user data");
+  }
+  };
+  useEffect(() => {
+  if (loading) return;
+  if (!user) return navigate("/");
+  fetchUserName();
+  }, [user, loading]);
   return (
     <section>
       <Layout/>
@@ -21,26 +47,26 @@ export default function Profile() {
         </div>
 
         <div class="bg-white px-8 py-6 pb-8 mb-4 bg-gray-100 ">
-        <div class="mb-6">
+        {/* <div class="mb-6">
             <label class="block mb-2 bg-general-colortext-sm font-medium text-gray-900 dark:text-gray-300">
               Username
             </label>
-          </div>
+          </div> */}
           <div class="mb-6">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Registered Email
+              Email: {user?.email}
             </label>
           </div>
 
           <div class="mb-6">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Name
+              Name: {name}
             </label>
           </div>
 
           <div class="mb-6">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-              Phone Number
+              Phone Number: {phone}
             </label>
           </div>
           </div>
