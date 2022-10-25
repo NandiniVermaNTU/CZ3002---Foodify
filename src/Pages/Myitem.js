c19db73f97ac408d106daa4db7f8980ff2510f82
Yen Duong <yenlinh.2902@gmail.com>
2022-10-25 21:04:43 +0800

    finalize view food item

--- src/Pages/Myitem.js
+++ src/Pages/Myitem.js
@@ -1,11 +1,94 @@
-import React, { useState } from "react";
-// import {useNavigate} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {Link, useNavigate, NavLink} from "react-router-dom";
 import "../PagesCSS/Home.css";
-import "../PagesCSS/Myitem.css"
+import "../PagesCSS/Myitem.css";
 import Layout from "../Components/Layout";
 
+import db from '../firebase/firebase';
+import { auth } from "../firebase/firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { query, collection, getDocs,  addDoc, where } from "firebase/firestore";
+
 function Myitem() {
-  return (
+    const [foodItems, setFoodItems] = useState(
+        [
+            {
+                "name": "",
+                "quanity": "",
+                "expiryDate": ""
+            }
+        ]
+    );
+    const [name , setName] = useState();
+    const [quantity , setQuantity] = useState();
+    const [expiryDate , setExpiryDate] = useState();
+
+    const [userName, setUserName] = useState();
+    const [userEmail, setUserEmail] = useState();
+    const [userID, setUserID] = useState();
+
+    const [user, loading, error] = useAuthState(auth);
+
+    const navigate = useNavigate();
+
+    // Fetch user data
+    const fetchUserName = async () => {
+        try {
+            const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+            const doc = await getDocs(q);
+            const data = doc.docs[0].data();
+            setUserName(data.name);
+            setUserEmail(data.email);
+            setUserID(data.uid);
+            // console.log(userName);
+            // console.log(userEmail);
+            // console.log(userID);
+        } catch (err) {
+            console.error(err);
+            alert("An error occured while fetching user data");
+        }
+    };
+
+    const fetchItems = async () => {
+        try {
+            const q = query(collection(db, "food-items"), where("userID", "==", user?.uid));
+            const doc = await getDocs(q);
+            // console.log("doc", doc.docs.data());
+            // const data = doc.docs[0].data();
+            // console.log(data.name);
+            // console.log(data.email);
+            // console.log(data.uid);
+            const items = doc.docs;
+            let itemDocs = items.map(item => item.data());
+            setFoodItems(itemDocs);
+            // console.log("itemdoc:", itemDocs);
+            // console.log("fooditem", foodItems);
+        } catch (err) {
+            console.error(err);
+            alert("An error occured while fetching user data");
+        }
+    };
+    
+    useEffect(() => {
+        if (loading) return;
+        if (!user) return navigate("/");
+        fetchUserName();
+    }, [user, loading]);
+
+    useEffect(() => {
+        if (loading) return;
+        if (!user) return navigate("/");
+        fetchItems();
+        // console.log(userName);
+        // console.log(userID);
+        // console.log(userEmail);
+        // console.log(name);
+        // console.log(quantity);
+        // console.log(expiryDate);
+        console.log(foodItems);
+    }, [userName, user, loading]);
+
+    return (
     <div>
         <Layout/>
         <div class="w-full px-8 pt-10 item-center">
@@ -17,8 +100,11 @@ function Myitem() {
                     </div>
                 </div>
                 <div class="px-6 pt-6 overflow-x-auto">
+
                     <table class="w-full whitespace-nowrap">
                         <tbody>
+                            {foodItems.length>0? foodItems.map(fooditem => (
+
                             <tr tabindex="0" class="focus:outline-none flex-auto">
                                 <td>
                                     <div class="flex items-center py-5">
@@ -27,7 +113,7 @@ function Myitem() {
                                         </div> */}
                                         <div class="pl-3">
                                             <div class="flex items-center text-sm leading-none">
-                                                <p class="font-semibold text-gray-800 dark:text-white pr-5">Chicken</p>
+                                                <p class="font-semibold text-gray-800 dark:text-white pr-5">{fooditem.quantity} {fooditem.name}</p>
                                                 {/* <p class="text-indigo-700 dark:text-indigo-400  ml-3">10/10/2022</p> */}
                                                 {/* <div class="flex items-center justify-center py-1 bg-orange-100 rounded-full">
                                                     <p class="text-xs font-semibold leading-none leading-3 text-orange-700 px-5">20/10/2022</p>
@@ -39,40 +125,12 @@ function Myitem() {
                                 </td>
                                 <td class="pl-16">
                                     <div class="flex items-center justify-center py-1 bg-orange-100 rounded-full">
-                                        <p class="text-xs font-semibold leading-none leading-3 text-orange-700 px-5">17/10/2022</p>
+                                        <p class="text-xs font-semibold leading-none leading-3 text-orange-700 px-5">{fooditem.expiryDate}</p>
                                     </div>
                                 </td>
                             </tr>
-                            
-                            
-                            <tr tabindex="0" class="focus:outline-none">
-                                <td>
-                                    <div class="flex items-center py-5">
-                                        {/* <div class="bg-gray-100 dark:bg-gray-800  rounded-sm p-2.5">
-                                           <img src="../Images/chicken-icon.jpg" alt="rice" />
-                                        </div> */}
-                                        <div class="pl-3">
-                                            <div class="flex items-center text-sm leading-none">
-                                                <p class="font-semibold text-gray-800 dark:text-white pr-5 ">Rice</p>
-                                                {/* <p class="text-indigo-700 dark:text-indigo-400 content-end px-5">10/10/2022</p> */}
-                                                {/* <div class="flex items-center justify-center py-1 bg-orange-100 rounded-full">
-                                                    <p class="text-xs font-semibold leading-none leading-3 text-orange-700 px-5">17/10/2022</p>
-                                                </div> */}
-                                            </div>
-                                            {/* <p class="text-xs md:text-sm leading-none text-gray-600 dark:text-gray-200  mt-2">Description of rice here</p> */}
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="pl-16">
-                                    {/* <div> */}
-                                    <div class="flex items-center justify-center py-1 bg-orange-100 rounded-full">
-                                        <p class="text-xs font-semibold leading-none leading-3 text-orange-700 px-5">17/10/2022</p>
-                                    </div>
-{/*                                         
-                                    </div> */}
-                                </td>
-                            </tr>
-                            
+                            )): <></>}
+
                         </tbody>
                     </table>
                 </div>
