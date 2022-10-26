779ae3b0678213f3f7f647158e81f265963e0736
Yen Duong <yenlinh.2902@gmail.com>
2022-10-27 05:35:51 +0800

    update recipe

--- src/Pages/Recipe.js
+++ src/Pages/Recipe.js
@@ -1,7 +1,12 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
+import {Link, useNavigate, NavLink} from "react-router-dom";
 
 import Axios from "axios";
 import Layout from "../Components/Layout.js";
+import db from '../firebase/firebase';
+import { auth } from "../firebase/firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { query, collection, getDocs,  addDoc, where, deleteDoc, doc } from "firebase/firestore";
 
 // const apiKey = "cd8ff5193bbd492ea78b3a1d3f177137";
 const apiKey = "7418462201954cc1aa005af58cd840b6";
@@ -16,15 +21,98 @@ const Recipe = () => {
     const [stepsInfo, setSteps] = useState([]);
   
     const [ingredients, setIngredients] = useState("")
-  
+
+    const [foodItems, setFoodItems] = useState(
+        [
+            {
+                "name": "",
+                "quanity": "",
+                "expiryDate": ""
+            }
+        ]
+    );
+    const [name , setName] = useState();
+    const [quantity , setQuantity] = useState();
+    const [expiryDate , setExpiryDate] = useState();
+
+    const [userName, setUserName] = useState();
+    const [userEmail, setUserEmail] = useState();
+    const [userID, setUserID] = useState();
+
+
+    const [user, loading, error] = useAuthState(auth);
+    const navigate = useNavigate();
+
+
+    const fetchUserName = async () => {
+        try {
+            const q = query(collection(db, "users"), where("uid", "==", user?.uid));
+            const doc = await getDocs(q);
+            const data = doc.docs[0].data();
+            setUserName(data.name);
+            setUserEmail(data.email);
+            setUserID(data.uid);
+            // console.log(userName);
+            // console.log(userEmail);
+            // console.log(userID);
+        } catch (err) {
+            console.error(err);
+            alert("An error occured while fetching user data");
+        }
+    };
+
+    const fetchItems = async () => {
+        try {
+            const q = query(collection(db, "food-items"), where("userID", "==", user?.uid));
+            const doc = await getDocs(q);
+            // console.log("doc", doc.docs.data());
+            // const data = doc.docs[0].data();
+            // console.log(data.name);
+            // console.log(data.email);
+            // console.log(data.uid);
+            const items = doc.docs;
+            let itemDocs = items.map(item => item.data().name);
+            setFoodItems(itemDocs);
+            // console.log("itemdoc:", itemDocs);
+            // console.log("fooditem", foodItems);
+        } catch (err) {
+            console.error(err);
+            alert("An error occured while fetching user data");
+        }
+    };
+
+    useEffect(() => {
+        if (loading) return;
+        if (!user) return navigate("/");
+        fetchUserName();
+    }, [user, loading]);
+
+    useEffect(() => {
+        if (loading) return;
+        if (!user) return navigate("/");
+        fetchItems();
+        // console.log(userName);
+        // console.log(userID);
+        // console.log(userEmail);
+        // console.log(name);
+        // console.log(quantity);
+        // console.log(expiryDate);
+        console.log(foodItems);
+    }, [userName, user, loading]);
+
+    let ings = foodItems.join(",+");
     const fetchRecipes = () => {
-      Axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}&number=5`).then((res) => {
+      Axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ings}&number=5`).then((res) => {
           console.log(res.data);
           console.log(res.data[0].id)
           setRecipes(res.data)
       })
     }
-  
+    
+    useEffect(()=>{
+        fetchRecipes();
+    }, [foodItems]);
+
     const fetchRecipesInfo = (id) => {
       Axios.get(`https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false?apiKey=${apiKey}`).then((res) => {
           setRecipes(res.data)
@@ -108,13 +196,6 @@ const Recipe = () => {
     //   }, []);
 
 
-
-    
-
-
-  
-  
-
   return (
     // <div>
     //   <button onClick={fetchData}>Generate recipes</button>
@@ -133,9 +214,10 @@ const Recipe = () => {
             <h2 class="text-3xl font-semibold text-gray-500 md:text-4xl italic py-5"><span class="font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-red-400 via-rose-500 to-yellow-400 font-bold italic">Magic</span> Recipes </h2>
         </div>
 
-        <div class="px-5">   
+        <div class="flex justify-center lg:justify-start px-5 italic">Recommendation of meals could be made from your food items</div>
 
-            {/* <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Your Email</label> */}
+        {/* <div class="px-5">   
+
             <div class="relative">
                 <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
@@ -154,9 +236,9 @@ const Recipe = () => {
                     Search
                 </button>
             </div>
-        </div>
+        </div> */}
 
-        <div class="pt-6 overflow-x-auto">
+        <div class="overflow-x-auto">
         {recipe.map(recipe => (
             <div class="w-full px-5 pt-5 item-center">
                 <div class="border rounded-lg border pb-6 border-gray-200 dark:border-gray-700 ">
